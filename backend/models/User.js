c8a8690579d64d360a4552e8a8125239160d14b8
Daniel Woo <dwoo@prosper.com>
2017-05-31 22:59:50 -0700

    add dotenv, setup mongo sandbox, user and controller

---
+++ backend/models/User.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  name: String,
+  phoneNumber: String,
+});
+
+mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User');
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
