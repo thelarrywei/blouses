cc87f64549c4c2f8c3e80b3d1340c30d524b763c
Daniel Woo <dwoo@prosper.com>
2017-06-02 01:09:40 -0700

    move sendWeeklySMS() to utils, that file needs a rename

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,10 @@
-const twilio = require('twilio');
-
 const app = require('./app');
-const whenIsTheGame = require('./util/utils');
+const { whenIsTheGame, sendWeeklySMS } = require('./util/utils');
 // TODO: v2 api should allow for creating users and games, meaning no seed data import just query db
-const { games, members } = require('./seed');
-const { accountSid, authToken, fromNumber } = require('./twilio_config');
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
-const client = new twilio(accountSid, authToken)
-
-const weeklySMS = (week) => {
-  let SMSBody;
-
-  const game = games[week];
-  const gameText = game.bye ? 'we have a bye this week' : `this week's game is on ${game.date} at ${game.time} reply IN, OUT, or MAYBE`;
-
-  members.forEach(({ name, phoneNumber }) => {
-    console.log(name, phoneNumber);
-    SMSBody = `Hey ${name}, ${gameText}`;
-    client.messages.create({
-      body: SMSBody,
-      to: phoneNumber,
-      from: fromNumber
-    }).then(response => {
-      console.log(response);
-    });
-  });
-
-};
-
-whenIsTheGame(weeklySMS);
+whenIsTheGame(sendWeeklySMS);
--- backend/util/utils.js
+++ backend/util/utils.js
@@ -1,4 +1,9 @@
 const moment = require('moment');
+const twilio = require('twilio');
+const { accountSid, authToken, fromNumber } = require('../twilio_config');
+const { games, members } = require('../seed');
+
+const client = new twilio(accountSid, authToken);
 
 const whenIsTheGame = sendSMS => {
   // PROD
@@ -23,4 +28,24 @@ const whenIsTheGame = sendSMS => {
 
 };
 
-module.exports = whenIsTheGame;
+const sendWeeklySMS = (week) => {
+  let SMSBody;
+
+  const game = games[week];
+  const gameText = game.bye ? 'we have a bye this week' : `this week's game is on ${game.date} at ${game.time} reply IN, OUT, or MAYBE`;
+
+  members.forEach(({ name, phoneNumber }) => {
+    console.log(name, phoneNumber);
+    SMSBody = `Hey ${name}, ${gameText}`;
+    client.messages.create({
+      body: SMSBody,
+      to: phoneNumber,
+      from: fromNumber
+    }).then(response => {
+      console.log(response);
+    });
+  });
+
+};
+
+module.exports = { whenIsTheGame, sendWeeklySMS };
