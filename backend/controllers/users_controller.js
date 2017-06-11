f3378331ede501f1b8a2e855b5b5b1716331b2e7
Daniel Woo <dwoo@prosper.com>
2017-06-11 01:47:45 -0700

    rearrange requires

--- backend/controllers/users_controller.js
+++ backend/controllers/users_controller.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
 const User = require('../models/user');
-module.exports = router;
+
+const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', (req, res) => {
   // NB: creating an API will be v2 so information can be modified via Postman
   res.status(200).send('hello world')
 });
+
+module.exports = router;
