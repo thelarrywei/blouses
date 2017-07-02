706a9d8b44a08b0eda9853053432844cfb8bc1ea
Daniel Woo <dwoo@prosper.com>
2017-07-02 14:24:47 -0700

    whitelist messages/reply

--- backend/util/middlewares.js
+++ backend/util/middlewares.js
@@ -1,5 +1,5 @@
 const checkAuth = function(req, res, next) {
-  if (req.path !== '/messages') {
+  if (req.path !== '/messages/reply') {
     if (req.headers.api_key !== process.env.API_KEY) return res.send('You are not authorized to perform this action');
   }
 
