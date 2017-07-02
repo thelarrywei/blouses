eff9e01998e8a3a59f3b7f198aaa09f5952c9001
Daniel Woo <dwoo@prosper.com>
2017-07-02 15:27:05 -0700

    comment dev code, uncomment prod code, add comment and define gameReminder function

--- backend/util/utils.js
+++ backend/util/utils.js
@@ -3,13 +3,15 @@ const { client, twilioNumber } = require('../twilio_config');
 const { games, users: members } = require('../seed');
 const { validStatuses } = require('./constants');
 
+// TODO: breakout whenIsTheGame, sendWeeklySMS, DEV_SMS, and gameReminder into a ScheduledTexts module?
+
 const whenIsTheGame = (sendSMS) => {
   // PROD
-  // const initialDelay = moment().weekday(7).diff(moment());
-  // const weeklyDelay = 6.048e+8;
+  const initialDelay = moment().weekday(7).diff(moment());
+  const weeklyDelay = 6.048e+8;
   // DEV
-  const initialDelay = 100;
-  const weeklyDelay = 100;
+  // const initialDelay = 100;
+  // const weeklyDelay = 100;
 
   let count = 0;
 
@@ -47,6 +49,10 @@ const DEV_SMS = () => {
     to: process.env.DEV_NUMBER,
     from: twilioNumber,
   });
+};
+
+const gameReminder = () => {
+
 };
 
 const isValidStatus = status => validStatuses.includes(status);
