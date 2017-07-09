61625bfd99d0f9fef87ce9c4ec6d6680f5ae4473
Daniel Woo <dwoo@prosper.com>
2017-07-09 00:17:32 -0700

    remove complete TODO comments

--- backend/models/game.js
+++ backend/models/game.js
@@ -16,7 +16,6 @@ const GameSchema = new Schema({
 
 GameSchema.statics.nextGame = function nextGame(cb) {
   // not sure why I can't pass these from utils... is it bc you can't pass a moment? even as a string? :(
-  // TODO: check if the wednesday calculated below has already passed, if passed use 10 next wednesday's game
   let start = moment.tz(process.env.MOMENT_LOCALE).weekday(3).startOf('day').toISOString();
   let end = moment(start).add(1, 'days').toISOString();
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 const { whenIsTheGame, sendWeeklySMS, DEV_SMS } = require('./util/utils');
-// TODO: v2 api should allow for creating users and games, meaning no seed data import just query db
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
