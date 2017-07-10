ecd23701e3dcec6dc0ae5239e625da1753957ce7
Daniel Woo <dwoo@prosper.com>
2017-07-09 18:16:53 -0700

    undo db config changes

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
-const mongo_uri = process.env.ENV === 'PROD' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_DEV;
 
-mongoose.connect(mongo_uri);
+mongoose.connect(process.env.MONGO_URI);
