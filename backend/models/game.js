544b23cba4600070ca4c891fa28adb10bfd8a3bd
Daniel Woo <dwoo@prosper.com>
2017-07-17 00:03:33 -0700

    adjust gameHasPassed

--- backend/models/game.js
+++ backend/models/game.js
@@ -41,14 +41,7 @@ GameSchema.statics.nextGame = function nextGame(cb) {
 };
 
 const gameHasPassed = function gameHasPassed(game) {
-  console.log(`${moment(game.date).diff(moment.tz(process.env.MOMENT_LOCALE))} milliseconds till game`);
-  console.log('game date: ', moment(game.date).toString());
-  console.log('tz game date: ', moment.tz(game.date, process.env.MOMENT_LOCALE).toString());
-  console.log('current date: ', moment().toString());
-  console.log('tz current date: ', moment.tz(process.env.MOMENT_LOCALE).toString());
-  const gameDate = moment(game.date);
-  console.log('sigh maybe this will have to do: ',moment.tz(game.date, process.env.MOMENT_LOCALE).weekday(3).startOf('day').hour(gameDate.hours()).minute(gameDate.minutes()).diff(moment()));
-  return moment.tz(game.date, process.env.MOMENT_LOCALE).weekday(3).startOf('day').hour(gameDate.hours()).minute(gameDate.minutes()).diff(moment()) < 0;
+  return moment.tz(game.date, process.env.MOMENT_LOCALE).diff(moment()) < 0;
 };
 
 mongoose.model('Game', GameSchema);
